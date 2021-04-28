12c7ed734f7308af538873bdcede2dc9a035408c
Noof <noof.mohammed998@gmail.com>
2021-04-28 19:25:50 +0300

    style icon

--- main.js
+++ main.js
@@ -186,7 +186,7 @@ const addressName = () => {
   morningDiv.on("click", () => {
     $(".thecer").hide();
     $("#morning").show();
-    funShow("morning");
+    createButtons("morning");
   });
 
   const eveningDiv = $("<div id='evening' class='thecer'>أذكار المساء</div>");
@@ -194,7 +194,7 @@ const addressName = () => {
   eveningDiv.on("click", () => {
     $(".thecer").hide();
     $("#evening").show();
-    funShow("evening");
+    createButtons("evening");
   });
 
   const sleepDiv = $("<div id='sleep' class='thecer'>أذكار النوم</div>");
@@ -202,7 +202,7 @@ const addressName = () => {
   sleepDiv.on("click", () => {
     $(".thecer").hide();
     $("#sleep").show();
-    funShow("sleep");
+    createButtons("sleep");
   });
 
   const weekUpDiv = $(
@@ -212,7 +212,7 @@ const addressName = () => {
   weekUpDiv.on("click", () => {
     $(".thecer").hide();
     $("#weekUp").show();
-    funShow("weekUp");
+    createButtons("weekUp");
   });
 
   const addDiv = $("<div class=add>أضافه أذكار</div>");
@@ -220,51 +220,45 @@ const addressName = () => {
   addDiv.on("click", () => {
     $(".thecer").hide();
     $("#add").show();
-    funShow("add");
+    createButtons("add");
   });
-};
+  };
 
-const funShow = (type) => {
+const createButtons = (type) => {
   let array = obj[type].list;
 
-const textContainer = $( `<div class='textContainer'>${array[0]}</div>`);
-main.append(textContainer);
-
-
-
-
-
+  const textContainer = $(`<div class='textContainer'>${array[0]}</div>`);
+  main.append(textContainer);
 
   const buttonContainer = $("<div class='buttonContainer'></div>");
-  const iconLeft = $(`<div class='clickLeft'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-square" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-</svg></>`);
-buttonContainer.html(iconLeft);
-
-  const iconRight = $(`<div class='clickRight'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+  const iconLeft = $(`<div class='clickLeft'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+  <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"/>
 </svg></div>`);
-buttonContainer.append(iconRight);
+  buttonContainer.html(iconLeft);
+  iconLeft.on("click", () => {
+    showThiker();
+  });
+  const iconRight = $(`<div class='clickRight'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+  <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
+</svg></div>`);
+  buttonContainer.append(iconRight);
+  iconRight.on("click", () => {
+    showThiker();
+  });
 
-
-
-
-main.append(buttonContainer);
-let index = 0;
-let array = obj[type].list;
-
-const nextText = () => {
-    index+=1
-   let text =  array[index]
-}
-
-
-//   for (let i = 0; i < array.length; i++) {
-//     let ul = $(".manyArray");
-    
-//     let li = $(`<li>${array[i]}</li>`);
-//     console.log(array[i])
-//     ul.append(li);
-//   }
+  main.append(buttonContainer);
 };
+
+let index = 0;
+
+const showThiker = () => {
+  for (let i = 0; i < arrayTheker.length; i++) {
+    array = obj[arrayTheker[i]].list;
+  }
+  console.log('-------------+++++')
+
+  index += 1;
+  let text = array[index];
+};
+
 
