88e047ed54e37f6ff1ce77e55c1e46d8c274b51f
Noof <noof.mohammed998@gmail.com>
2021-12-09 16:32:34 +0200

    update data

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ let obj = {
 
       `ياحي يا قيوم برحمتك أستغيث أصلح لي شأني كله ولا تكلني إلى نفسي طـرفة عين. `,
 
-      `أمسينا وامسى المـلك لله رب العـالمـين ، اللهـم إنـي أسـألـك خـير هـذا الـيوم ، فـتحه ، ونصـره ، ونـوره وبـركتـه ، وهـداه ، وأعـوذ بـك مـن شـر ما فـيه وشـر ما بعـده. `,
+      `أمسينا وامسى المـلك لله رب العـالمـين ، اللهـم إنـي أسـألـك خـير هـذا الليله ، فتحها ، ونصرها ، ونـورها وبـركتـها ، وهـداها ، وأعـوذ بـك مـن شـر ما فـيها وشـر ما بعـدها. `,
 
       `أمسينا على فطرة الإسلام، وعلى كلمة الإخلاص، وعلى دين نبينا محمد صلى الله عليه وسلم، وعلى ملة أبينا إبراهيم حنيفا مسلما وما كان من المشركين. `,
 
@@ -219,7 +219,7 @@ const addressName = () => {
   addDiv.on("click", () => {
     $(".theker").hide();
     $("#add").show();
-    $(".container").hide()
+    $(".container").hide();
     addTheker();
   });
 
@@ -253,7 +253,8 @@ const createButtons = (type) => {
   container.html(textContainer);
 
   const buttonContainer = $("<div class='buttonContainer'></div>");
-  const iconLeft = $(`<div class='clickLeft'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+  const iconLeft =
+    $(`<div class='clickLeft'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
   <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"/>
 </svg></div>`);
   buttonContainer.html(iconLeft);
@@ -264,7 +265,8 @@ const createButtons = (type) => {
     index -= 1;
     showThiker(array, index);
   });
-  const iconRight = $(`<div class='clickRight'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+  const iconRight =
+    $(`<div class='clickRight'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
   <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
 </svg></div>`);
   buttonContainer.append(iconRight);
